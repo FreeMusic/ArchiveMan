07a33d63c064fc94fac991f6cbfda85e9d6fc6b3
958589819@qq.com <958589819@qq.com>
2020-10-28 16:31:15 +0800

    我们添加了背景颜色

--- ArchiveTest/ViewController.m
+++ ArchiveTest/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIView *view = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:view];
+    view.backgroundColor = [UIColor redColor];
 }
 
 
